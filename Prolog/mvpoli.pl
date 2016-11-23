48a39bacfc0633f3fe86096073c4ae4db6b6f1a2
Cristian Baldi <bld.cris.96@gmail.com>
2016-11-23 12:50:17 +0100

    Prolog: add a comment

--- Prolog/mvpoli.pl
+++ Prolog/mvpoli.pl
@@ -117,6 +117,7 @@ compute_total_degree_for_vars_powers([v(Power,_)|Other], TotalDegree) :-
 % and let us focus on the logic
 % we don't provide an equality delta predicate (= is always false) since we cover all the cases with < and >
 % This is so we keep duplicates, which are usually deleted by predsort  
+% compress_sorted_vps/3 will take care of the rest
 compare_vars_powers(<, v(_E1, V1),v(_E2, V2)) :-
 	V1@=<V2, %<equals so we keep duplicates of the same variable, predosort usually deletes equals elements
 	!.
