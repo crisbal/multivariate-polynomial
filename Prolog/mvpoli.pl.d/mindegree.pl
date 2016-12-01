91a5866b704c2f2f9b18c7b662c694fbfb7e6ef5
Cristian Baldi <bld.cris.96@gmail.com>
2016-12-01 17:47:21 +0100

    Prolog: allow empty polynomial in max/min degree predicates

--- Prolog/mvpoli.pl.d/mindegree.pl
+++ Prolog/mvpoli.pl.d/mindegree.pl
@@ -1,13 +1,14 @@
 %% mindegree/2
 % TODO: disallow two-way
-mindegree(poly([m(_, TD, _)|Monomials]),Degree) :-
-  mindegree_worker([m(_, TD, _)|Monomials],TD,Degree),
+mindegree(poly([]), 0) :- !.
+mindegree(poly([m(_, TD, _) | Monomials]), Degree) :-
+  mindegree_worker([m(_, TD, _) | Monomials], TD, Degree),
   !.
 
-mindegree_worker([],CurrentMinDegree,CurrentMinDegree) :- !.
-mindegree_worker([m(_, TD, _)|Monomials],CurrentMinDegree,Degree) :-
+mindegree_worker([], CurrentMinDegree, CurrentMinDegree) :- !.
+mindegree_worker([m(_, TD, _) | Monomials], CurrentMinDegree, Degree) :-
   TD < CurrentMinDegree,
-  mindegree_worker(Monomials,TD,Degree).
-mindegree_worker([m(_, _, _)|Monomials],CurrentMinDegree,Degree) :-
-  mindegree_worker(Monomials,CurrentMinDegree,Degree).
+  mindegree_worker(Monomials, TD, Degree).
+mindegree_worker([m(_, _, _) | Monomials], CurrentMinDegree, Degree) :-
+  mindegree_worker(Monomials, CurrentMinDegree, Degree).
 
