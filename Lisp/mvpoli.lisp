157464f7ea245cadecd9b7722e26c45871ce8c99
Cristian Baldi <bld.cris.96@gmail.com>
2016-12-08 11:55:00 +0100

    Lisp: first implementation of basic as monomial function

--- Lisp/mvpoli.lisp
+++ Lisp/mvpoli.lisp
@@ -1,17 +1,71 @@
-(defun as-varpower(vp-expression-list)
-  (if (and (listp vp-expression-list)
-	   (equal (first vp-expression-list) 'expt)
-	   (symbolp (second vp-expression-list))
-	   (numberp (third vp-expression-list)))
-      (list 'V (second vp-expression-list) (third vp-expression-list))))
+(defun exptp(something)
+  "T is returned when SOMETHING is a list in the form '(expt symbol number)"
+  (and (listp something)
+       (equal (first something) 'expt)
+       (symbolp (second something))
+       (numberp (third something))))
 
-(defun to-monomial-component(element)
-  (cond ((numberp element) element)
-	((symbolp element) (as-varpower (list 'expt element '1)))
-	((as-varpower element) (as-varpower element))
-	(T (error "What is that parsed thing?"))))
+(defun expression-variable-p(something)
+  "Something is either a symbol or an '(expt symbol number) object"
+  (or (exptp something)
+      (and (symbolp something)
+	   (not (null something)))))
 
-(defun as-monomial(monomial-expression)
-  (if (eq (first monomial-expression) '*)
-      (mapcar 'to-monomial-component (rest monomial-expression))
-      (error "Invalid monomial")))
+(defun monomial-expression-component-p(component)
+  "T is returned when COMPONENT is a valid expression component.
+COMPONENT is either a number or something validated by EXPRESSION-VARIABLE-P"
+  (or (numberp component)
+      (expression-variable-p component)))
+
+(defun monomial-expression-p(expression)
+  "Validate EXPRESSION as a valid monomial expression.
+EXPRESSION must begin with '* and must include only elements validated by MONOMIAL-EXPRESSION-COMPONENT-P
+TODO: a numberp is a valid monomial"
+  (and (listp expression)
+       (equal (first expression) '*)
+       (every #'identity (mapcar 'monomial-expression-component-p (rest expression))))) ;; check that all symbols are valid
+
+(defun coefficients(expression)
+  "Return all the coefficient inside EXPRESSION"
+  (remove-if-not #'numberp expression))
+
+(defun total-coefficient(expression)
+  "Compute the total coefficient of EXPRESSION.
+This is achived by multiplying all the coefficients of EXPRESSION"
+  (reduce '* (coefficients expression))) ;; default value can be omitted since for '* is 1
+
+(defun expression-variables(expression)
+  "Return all the variables inside EXPRESSION"
+  (remove-if-not #'expression-variable-p expression))
+
+(defun expression-variable-to-varpower(exp-var)
+  "Convert EXP-VAR (validated by EXPRESSION-VARIABLE-P) to an object in the format of '(v exponent variable)"
+  (when (expression-variable-p exp-var) ;; let's make sure
+    (let ((vp-variable (if (listp exp-var)
+			   (second exp-var)
+			   exp-var))
+	  (vp-exponent (if (listp exp-var)
+			   (third exp-var)
+			   1)))
+      (list 'v vp-exponent vp-variable))))
+
+(defun varpowers(expression)
+  "Return the varspowers inside EXPRESSION.
+This casts the list gotted by EXPRESSION-VARIABLES."
+  (mapcar #'expression-variable-to-varpower (expression-variables expression)))
+
+(defun build-monomial-object(coefficient total-degree varpowers)
+  "Given all the details builds the monomial as needed by the requirements"
+  (list 'm coefficient total-degree varpowers))
+
+(defun parse-monomial-expression(expression)
+  (let ((e-coefficient (total-coefficient expression))
+	(e-varpowers (varpowers expression));;(parse-variables expression))
+	(e-total-degree 'total-degree));(compute-total-degree vars-powers)))
+    (build-monomial-object e-coefficient e-total-degree e-varpowers)))
+
+(defun as-monomial(expression)
+  "TODO: a number is a monomial"
+  (if (monomial-expression-p expression)
+      (parse-monomial-expression (rest expression))
+      (error "Invalid expression")))
