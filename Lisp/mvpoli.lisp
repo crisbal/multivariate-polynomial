59f6fcc99c429e18eb3b25eeadc5f93902bae019
Cristian Baldi <bld.cris.96@gmail.com>
2016-12-09 14:48:04 +0100

    Lisp: support for all coefficients and sort vps

--- Lisp/mvpoli.lisp
+++ Lisp/mvpoli.lisp
@@ -1,3 +1,10 @@
+(defun eval-as-number(expression)
+  "Eval EXPRESSION and return its numeric value"  
+  (let ((result (handler-case (eval expression)
+		  (error () nil)
+		  (warning () nil))))
+    (if (numberp result) result nil)))
+
 (defun exptp(something)
   "T is returned when SOMETHING is a list in the form '(expt symbol number)"
   (and (listp something)
@@ -15,7 +22,9 @@
   "T is returned when COMPONENT is a valid expression component.
 COMPONENT is either a number or something validated by EXPRESSION-VARIABLE-P"
   (or (numberp component)
-      (expression-variable-p component)))
+      (numberp (eval-as-number component))
+      (expression-variable-p component))
+  )
 
 (defun monomial-expression-p(expression)
   "Validate EXPRESSION as a valid monomial expression.
@@ -27,12 +36,12 @@ TODO: a numberp is a valid monomial"
 
 (defun coefficients(expression)
   "Return all the coefficient inside EXPRESSION"
-  (remove-if-not #'numberp expression))
+  (remove-if-not (lambda (x) (numberp (eval-as-number x))) expression))
 
 (defun total-coefficient(expression)
   "Compute the total coefficient of EXPRESSION.
 This is achived by multiplying all the coefficients of EXPRESSION"
-  (reduce '* (coefficients expression))) ;; default value can be omitted since for '* is 1
+  (reduce #'* (mapcar #'eval-as-number (coefficients expression)))) ;; default value can be omitted since for '* is 1
 
 (defun expression-variables(expression)
   "Return all the variables inside EXPRESSION"
@@ -49,10 +58,36 @@ This is achived by multiplying all the coefficients of EXPRESSION"
 			   1)))
       (list 'v vp-exponent vp-variable))))
 
-(defun varpowers(expression)
+(defun varpower-power(varpower)
+  (when (and (listp varpower)
+	     (numberp (second varpower)))
+    (second varpower)))
+
+(defun varpower-symbol(varpower)
+  (when (and (listp varpower)
+	     (symbolp (third varpower)))
+    (third varpower)))
+
+(defun is-varpower(varpower)
+  (and (listp varpower)
+       (eq 'v (first varpower))
+       (let ((p (varpower-power varpower))
+	     (s (varpower-symbol varpower)))
+	 (and (integerp p)
+	      (symbolp s)))))
+
+(defun varpowers-from-expression(expression)
   "Return the varspowers inside EXPRESSION.
 This casts the list gotted by EXPRESSION-VARIABLES."
   (mapcar #'expression-variable-to-varpower (expression-variables expression)))
+
+(defun sort-varpowers(varpowers)
+  "Sort VARPOWERS.
+VARPOWERS is a list of items validated by is-varpower"
+  (when (and (listp varpowers)
+	     (every #'identity (mapcar #'is-varpower varpowers)))
+    (sort varpowers (lambda(vp1 vp2)
+		      (string< (varpower-symbol vp1) (varpower-symbol vp2))))))
 
 (defun build-monomial-object(coefficient total-degree varpowers)
   "Given all the details builds the monomial as needed by the requirements"
@@ -60,7 +95,7 @@ This casts the list gotted by EXPRESSION-VARIABLES."
 
 (defun parse-monomial-expression(expression)
   (let ((e-coefficient (total-coefficient expression))
-	(e-varpowers (varpowers expression));;(parse-variables expression))
+	(e-varpowers (sort-varpowers (varpowers-from-expression expression)));;(parse-variables expression))
 	(e-total-degree 'total-degree));(compute-total-degree vars-powers)))
     (build-monomial-object e-coefficient e-total-degree e-varpowers)))
 
