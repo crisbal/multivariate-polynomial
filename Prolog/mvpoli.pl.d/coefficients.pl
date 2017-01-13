02e1a90d0a14988ce52600a98efd741041380e06
Cristian Baldi <bld.cris.96@gmail.com>
2017-01-13 14:54:02 +0100

    Prolog: is_monomial also check varpowers, null polynomial has 0 coefficient

--- Prolog/mvpoli.pl.d/coefficients.pl
+++ Prolog/mvpoli.pl.d/coefficients.pl
@@ -5,10 +5,11 @@
 coefficients(GenericPoly, Coefficients) :-
     nonvar(GenericPoly), % you can't go backwards
     to_polynomial(GenericPoly, poly(Monomials)), % "cast" to polynomial type
-	coefficients_worker(Monomials, Coefficients).
+    coefficients_worker(Monomials, Coefficients).
 
-coefficients_worker([], []).
+coefficients_worker([], [0]) :- !.
+coefficients_worker([m(Coefficient, _Degree, _VarsPowers)], [Coefficient]) :- !.
 coefficients_worker([m(Coefficient, _Degree, _VarsPowers) | RestOfMonomials], 
         [Coefficient | RestOfCoefficients]) :-
-	coefficients_worker(RestOfMonomials, RestOfCoefficients).
-
+    coefficients_worker(RestOfMonomials, RestOfCoefficients),
+    !.
--- Prolog/mvpoli.pl.d/is_monomial.pl
+++ Prolog/mvpoli.pl.d/is_monomial.pl
@@ -7,6 +7,7 @@ is_monomial(m(_C, TD, VPs)) :-
 	% handle_zero_coefficient(C, VPs, VPs), %TODO: is needed?
 	compute_total_degree_for_vars_powers(VPs, TD), %TODO: is needed?
 	is_list(VPs),
+	foreach(member(V, VPs), is_varpower(V)),
 	% predsort(compare_vars_powers, VPs, VPs), %TODO: is needed?
-	% compress_sorted_vps(VPs, VPs), %TODO: is needed
+	% compress_sorted_vps(VPs, VPs), %TODO: is needed?
 	!.
