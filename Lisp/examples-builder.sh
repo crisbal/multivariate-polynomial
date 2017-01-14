20a492df8cd8f22c5cf8a1c7700837335128ec12
Toma Luca <toma.luca95@gmail.com>
2017-01-14 11:11:19 +0100

    Lisp: some updates for examples-builder.sh
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- Lisp/examples-builder.sh
+++ Lisp/examples-builder.sh
@@ -50,36 +50,65 @@ do
   c=$(($c+1))
 done
 
-if false ; then
-fns[$c]="(monotimes )"
+fns[$c]="(monotimes (to-monomial '(* x f a)) (to-monomial '(* x w)))"
 c=$(($c+1))
-fns[$c]="(polyval )"
-c=$(($c+1))
-fns[$c]="(maxdegree )"
-c=$(($c+1))
-fns[$c]="(mindegree )"
-c=$(($c+1))
-fns[$c]="(coefficients )"
-c=$(($c+1))
-fns[$c]="(variables )"
-c=$(($c+1))
-fns[$c]="(monomials )"
-c=$(($c+1))
-fns[$c]="(varpowers )"
-c=$(($c+1))
-fns[$c]="(vars-of )"
-c=$(($c+1))
-fns[$c]="(monomial-degree )"
-c=$(($c+1))
-fns[$c]="(monomial-coefficient )"
+fns[$c]="(monotimes (to-monomial '(* x f a)) (to-monomial '(* x (expt f 3) a)))"
 c=$(($c+1))
 
-fns[$c]="(pprint-polynomial )"
+fns[$c]="(polyval '(* x y z) '(1 2 3))"
 c=$(($c+1))
-fns[$c]="(pprint-monomial )"
+fns[$c]="(polyval '(* x y z) '(4 2 3))"
 c=$(($c+1))
-fi
+fns[$c]="(polyval '(* x (expt y 10) z) '(1 2 3))"
+c=$(($c+1))
 
+polys=(
+  "'(+ (* x (expt y 10) z) (* y (expt x 10) q))"
+  "'(* x (expt y 10) z)"
+  "'(* 10 (expt y 10) z)"
+)
+for poly_n in $(seq 0 $((${#polys[@]}-1)))
+do
+  fns[$c]="(maxdegree ${polys[$poly_n]})"
+  c=$(($c+1))
+
+  fns[$c]="(mindegree ${polys[$poly_n]})"
+  c=$(($c+1))
+
+  fns[$c]="(coefficients ${polys[$poly_n]})"
+  c=$(($c+1))
+
+  fns[$c]="(variables ${polys[$poly_n]})"
+  c=$(($c+1))
+
+  fns[$c]="(monomials ${polys[$poly_n]})"
+  c=$(($c+1))
+
+  echo "(pprint-polynomial ${polys[$poly_n]})"
+done
+
+
+monoms=(
+  "'(* x (expt y 10) z)"
+  "'(* (expt x 4) (expt y 10) z)"
+  "'(* (expt x 4) (expt y 10) (expt z 3))"
+)
+for monom_n in $(seq 0 $((${#monoms[@]}-1)))
+do
+  fns[$c]="(varpowers ${monoms[$monom_n]})"
+  c=$(($c+1))
+
+  fns[$c]="(vars-of ${monoms[$monom_n]})"
+  c=$(($c+1))
+
+  fns[$c]="(monomial-degree ${monoms[$monom_n]})"
+  c=$(($c+1))
+
+  fns[$c]="(monomial-coefficient ${monoms[$monom_n]})"
+  c=$(($c+1))
+
+  echo "(pprint-monomial ${monoms[$monom_n]})"
+done
 
 tmp_dir=`mktemp -d`"/"
 
@@ -89,7 +118,11 @@ while [[ $i -lt $c ]]
 do
   fn_name=$(echo ${fns[$i]} | awk '{ print $1 }' | cut -d "(" -f 2)
   echo "Running "${fns[$i]}
-  echo \`${fns[$i]}\` ritornerà \`$(sbcl --noinform --load mvpoli.lisp --eval "(pprint ${fns[$i]})" --eval "(quit)")\` >> $tmp_dir$fn_name
+  if test ! -f $tmp_dir$fn_name
+  then
+    echo "Ad esempio:" > $tmp_dir$fn_name
+  fi
+  echo \`${fns[$i]}\` ritornerà \`$(sbcl --noinform --load mvpoli.lisp --eval "(pprint ${fns[$i]})" --eval "(quit)")\` | sed 's/` (/`(/g' >> $tmp_dir$fn_name
   i=$(($i+1))
 done
 
